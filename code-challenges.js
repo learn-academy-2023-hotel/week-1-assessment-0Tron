b493c37126c65835494482cc4629409bdf23ffba
oscartroncoso <oscarrd8@gmail.com>
2023-10-15 20:48:46 -1000

    finished week 1

--- code-challenges.js
+++ code-challenges.js
@@ -10,8 +10,8 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a conditional statement that evaluates two strings and determines which of the strings has more characters. Use the two sets of test variables provided.
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
 // Expected outcome: "banana"
 
 // Set two:
@@ -24,22 +24,63 @@ const fruit2 = "banana"
 
 // --------------------1) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
-const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
-const padres1998WorldSeriesRuns = [6, 3, 5, 3]
-// Expected output: 9
+// const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
+// const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// // Expected output: 9
+
+const combined = padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns).length
+console.log(combined)
 
 // Pseudo code:
+
+// - first we have to combine the two arrays with the mutant concat
+// - then we add .lenght at the end to get the total of the two combined arrays
+// - we use console.log to give us the result of the lenght witch in this case is 9
 
 // --------------------2) Create the code that will reverse the letters of a string using the test variable provided below.
 
-const currentCohort = "Golf 2023"
+var currentCohort = ["Golf 2023"]
+
+var stringToArray = currentCohort.split()
+console.log(stringToArray)
+
+var stringToArray = currentCohort.split("")
+console.log(stringToArray)
+
+stringToArray.reverse()
+console.log(stringToArray)
+
 // Expected output: "3202 floG"
 
 // Pseudo code:
+
+// Reverse in javascript cant be applied to a string, but it can be applied to an array
+
+// - Var currentCohort selects the variable im selecting witch is Gold 2023
+
+// - i tried to convert the string into an area using the split method.
+
+// - i then tried to split up the string into every single character being on its own
+
+// - I then tried to reverse the array
+
+// - i could not get it to Worker, it kept telling me .split was not a function. 
 
 // --------------------3) Create the code that will log only the odd numbers from the array using the test variable provided below.
 
 const stockExchange = [13, 34, -4, 42, 5, -5, 10, 27, 42, 10]
 // Expected output: 13 5 -5 27
 
+for (const number of stockExchange) {
+    if (number % 2 !== 0) {
+      console.log("ODD:", number);
+    }
+  }
+
+
 // Pseudo code:
+
+// - we create a loop function 
+// - the for function is looking through every number that we have written in the stockExchange Array
+// - the if function checks to see if its an ODD number. We do this by seeing if the number divided (%) by 2 is not equal to 0.
+// the console.log sends a line to us if the statement is true in the above line. if it is we get the ODD: (Numbeer)
--- coding-concepts.js
+++ coding-concepts.js
@@ -18,16 +18,17 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will give us the lenght every single character
+// b) Verify and explain: the output was 10 which is the lenght of every character including the space.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will choose the 4th letter 
+// b) Verify and explain: the output was the 4th letter, i thought it would have been l but i forgot that we start with 0.
+// outcome was = o
 
 // --------------------3) What will this log?
 
@@ -35,21 +36,21 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: The outcome should be the first index, outcome should be ruby
+// b) Verify and explain: i was correct with my prediction.
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this should make the weekndDays all upper case letters.
+// b) Verify and explain: the result was an error.  .toUpperCase is not a function for for strings.
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This should give us the result of 4 since there is 4 items to choose from
+// b) Verify and explain:the result was "number". the reason why i think it gave us this is because the typeof was lenght. lenght is associated with mumbers, i figured this out by hovering over lenght on the console line.
